9d952423ec19d327a5bb069c1aaffc4c7678f079
tormariu <tormariu@1b3b20ba-2285-11df-88ee-e13ad67854f8>
2010-03-11 01:49:43 +0000

    brenn i helvete da jævla javafaen

--- trunk/src/CreateGUI.java
+++ trunk/src/CreateGUI.java
@@ -4,6 +4,8 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionListener;
 
 
 import javax.swing.JFrame;
@@ -15,59 +17,33 @@ public class CreateGUI extends JFrame implements KeyListener, ActionListener{
 	
 	public CreateGUI(){
 		this.setTitle("Dance Dance Revolution - 201B Edition");
-		this.setSize(800, 600);
-		this.setLocation(300, 100);
+		this.setSize(800, 600);		 	// sets the size of the windows
+		this.setLocation(300, 100); 	// where it appears on the screen at start-up
 		this.setVisible(true);
-		this.repaint();
+		//this.repaint();
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.setFocusableWindowState(true);
-		this.setFocusable(true);
-		this.addKeyListener(this);
+		this.setFocusable(false);
+		//this.addKeyListener(this); denne skal være i Arrow mest sannsynlig, tror jeg
 		cp = getContentPane();
-		cp.setBackground(Color.white);
+		//cp.setBackground(Color.red);
 	}
 	
-	static CreateGUI GUI = new CreateGUI();
-	
-	public static void createArrows(Arrow.Direction dir) throws InterruptedException{
-		Arrow rightarrow = new Arrow(dir);
-		JLayeredPane panel = new JLayeredPane();
-		panel.setSize(800, 600);
-		panel.setVisible(true);
-		panel.setLocation(0, 0);
-		panel.add(rightarrow);
-		panel.setFocusable(true);
-		GUI.getContentPane().add(panel);
-		
-		for(int i=600;i>-100;i--){
-			Thread.sleep(2);
-			rightarrow.setYpos(i);
-		}	
-	}
-	
-	public static void main(String[] args) throws InterruptedException{
-		createArrows(Arrow.Direction.DOWN);
-		createArrows(Arrow.Direction.UP);
-		createArrows(Arrow.Direction.LEFT);
-		createArrows(Arrow.Direction.RIGHT);
-	}
-
 	@Override
 	public void keyPressed(KeyEvent e) {
-		switch(e.getKeyCode()){
+		/*switch(e.getKeyCode()){
 			case KeyEvent.VK_LEFT:
 				System.out.println("Trykker venstre!");
 				break;
 			case KeyEvent.VK_UP:
-				System.out.println("Trykker høyre!");
+				System.out.println("Trykker opp!");
 				break;
 			case KeyEvent.VK_DOWN:
 				System.out.println("Trykker ned!");
 				break;
 			case KeyEvent.VK_RIGHT:
-				System.out.println("Trykker opp!");
+				System.out.println("Trykker høyre!");
 				break;
-		}
+		}*/
 	}
 
 	@Override
@@ -76,10 +52,10 @@ public class CreateGUI extends JFrame implements KeyListener, ActionListener{
 
 	@Override
 	public void keyTyped(KeyEvent e) {
-		
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
+		//System.out.println("balle");
 	}
 }
--- trunk/src/Runner.java
+++ trunk/src/Runner.java
@@ -3,18 +3,54 @@ public class Runner extends Thread {
 
 	public static void main(String[] args) {
 		(new Thread(new Runner())).start();
-		(new Thread(new Runner())).start();
-		(new Thread(new Runner())).start();
 	}
 
 	public void run(){
 		try {
-			CreateGUI.createArrows(Arrow.Direction.LEFT);
-			CreateGUI.createArrows(Arrow.Direction.RIGHT);
+			CreateGUI gui = new CreateGUI();
+			Arrow left = new Arrow(Arrow.Direction.LEFT);
+			Arrow up = new Arrow(Arrow.Direction.UP);
+			Arrow down = new Arrow(Arrow.Direction.DOWN);
+			Arrow right = new Arrow(Arrow.Direction.RIGHT);
+
+
+			Panel panel = new Panel();
+					
+			panel.setSize(800, 600);
+			panel.setLocation(0, 0);			
+			panel.add(right, new Integer(2));
+	
+			
+			gui.getContentPane().add(panel);
+			
+			//gui.add(panel);
+			
+		//	gui.add(left);
+//			gui.add(up);
+//			gui.add(down);
+//			gui.add(right);
+//			
+		
+			left.revalidate();
+			up.revalidate();
+			down.revalidate();
+			right.revalidate();
+		
+			
+//			JLayeredPane panel = new JLayeredPane();
+//			panel.setSize(800, 600);
+//			panel.setVisible(true); //denne har jo ikke en dritt å si tydeligvis
+//			panel.setLocation(0, 0);
+//			panel.add(up_arrow, 2);
+//			panel.setOpaque(true);
+//			panel.repaint();
+//			//panel.setFocusable(false);
+			
+//			gui.getContentPane().add(up_arrow);
+			
+
 		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
-	
 }
