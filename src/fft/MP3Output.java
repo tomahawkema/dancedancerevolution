f2d8fb0df497fe719bbb96c7c0dc01bb3ec13a6d
tormariu <tormariu@1b3b20ba-2285-11df-88ee-e13ad67854f8>
2010-03-12 09:03:10 +0000

    printout whenever the sample number exceeds a fixed limit

--- src/fft/MP3Output.java
+++ src/fft/MP3Output.java
@@ -14,12 +14,15 @@ public class MP3Output
 	public static void main( String[] argv ) throws FileNotFoundException, Exception
 	{			
 		AudioDevice device = new AudioDevice( );
-		MP3Decoder decoder = new MP3Decoder( new FileInputStream( "samples/mozart.mp3" ) );
+		MP3Decoder decoder = new MP3Decoder( new FileInputStream( "mp3s/jazz.mp3" ) );
 		float[] samples = new float[1024];
-		
+		int i = 0;
 		while( decoder.readSamples( samples ) > 0 )
 		{
 			device.writeSamples( samples );
+			if(samples[i] > 0.15)
+				System.out.println(i);
+			i++;
 		}
 	}
 }
