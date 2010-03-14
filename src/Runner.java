3bb9075c96109debc60cdaeaab37a5369d5a58c0
tormariu <tormariu@1b3b20ba-2285-11df-88ee-e13ad67854f8>
2010-03-14 05:11:50 +0000

    arrows are now generated automatically from the songz.

--- src/Runner.java
+++ src/Runner.java
@@ -1,9 +1,10 @@
 import java.awt.event.KeyListener;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.JButton;
 import javax.swing.JLayeredPane;
-import ddf.minim.*;
-import ddf.minim.analysis.*;
 
 public class Runner extends Thread {
 
@@ -16,41 +17,54 @@ public class Runner extends Thread {
 			CreateGUI gui = new CreateGUI();
 			gui.setFocusable(false);
 			
-			Arrow left = new Arrow(Arrow.Direction.LEFT);
-			Thread.sleep(2000);
-			Arrow up = new Arrow(Arrow.Direction.UP);
-			Thread.sleep(2000);
-			Arrow down = new Arrow(Arrow.Direction.DOWN);
-			Thread.sleep(2000);
-			Arrow right = new Arrow(Arrow.Direction.RIGHT);
-
-	
-			JButton knapp = new JButton("DETTE ER EN SABLA BRA KNAPP");
-			knapp.setSize(300, 20);
-			knapp.setLocation(400, 400);
+			List<Float> peaks = new Threshold().getPeaks();
 			
+			JLayeredPane panel = new JLayeredPane();
+			panel.setFocusable(false);			
+			
+			System.out.println("HER SKAL DET KOMME UT NOE BRAAA:");
+			for( int i = 0; i < peaks.size() - 1; i++ ){
+//		    	System.out.println(peaks.get(i));
+				if(peaks.get(i)>50){
+//					System.out.println(i);
+//					System.out.println(peaks.get(i));
+					float f = peaks.get(i);
+					panel.add(new Arrow((int)f%4, i*3));
+				}
+			}
+			System.out.println("PEAKS SIZE I RUNNER: " + peaks.size());
+				
+			
+//			JButton knapp = new JButton("DETTE ER EN SABLA BRA KNAPP");
+//			knapp.setSize(300, 20);
+//			knapp.setLocation(400, 400);
+//			
 			LoadMP3 load = new LoadMP3();
 			load.setLocation(600,100);
 			load.setSize(100,20);
-			
-			JLayeredPane panel = new JLayeredPane();
-			panel.setFocusable(false);
-			panel.add(load);
-			panel.add(knapp);
+//			
+//			panel.add(load);
+//			panel.add(knapp);
 			panel.setSize(800, 600);
-			panel.add(right);
-			panel.add(left);
-			panel.add(up);
-			panel.add(down);
+//			panel.add(right);
+//			panel.add(left);
+//			panel.add(up);
+//			panel.add(down);
 			
 			gui.getContentPane().add(panel);
 
-			left.revalidate();
-			up.revalidate();
-			down.revalidate();
-			right.revalidate();
+//			left.revalidate();
+//			up.revalidate();
+//			down.revalidate();
+//			right.revalidate();
 			
 		} catch (InterruptedException e) {
+			e.printStackTrace();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
